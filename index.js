4d9664283e65d1ea94119215d0483558de1e466b
Aaron Trovillion <aaron.trovillion@gmail.com>
2021-11-29 16:02:24 -0500

    Cut back for attempt to use xml2json with xml

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
 import axios from 'axios';
+import xml2json from 'xml2json';
 
 const userDiv = document.querySelector('.user');
 
 const baseEndpoint = 'https://www.boardgamegeek.com/xmlapi2/';
 const proxy = `https://cors-anywhere.herokuapp.com/`;
-
-export function showUser(data) {
-  console.log(data);
-  const userData = data.getElementsByTagName('firstname');
-  const firstName = userData[0].attributes.value.value;
-  const lastName = data.getElementsByTagName('lastname')[0].attributes.value
-    .value;
-  const userNameElement = document.createElement('p');
-  const userName = document.createTextNode(`${firstName} ${lastName}`);
-  userDiv.appendChild(userName);
-}
+const query = `user?name=kernowekya`;
 
 export async function xmlRequest() {
   axios.create({
-    baseURL: `https://cors-anywhere.herokuapp.com/https://www.boardgamegeek.com/xmlapi2/user?name=kernowekya`,
+    baseURL: `${proxy}${baseEndpoint}${query}`,
     timeout: 1000,
   });
-  axios
-    .get(
-      `https://cors-anywhere.herokuapp.com/https://www.boardgamegeek.com/xmlapi2/user?name=kernowekya`
-    )
-    .then(xml => {
-      const doc = new DOMParser().parseFromString(xml.data, 'text/xml');
-      showUser(doc);
-    });
+  axios.get(`${baseEndpoint}${query}`).then(xml => console.log(xml));
 }
 
 xmlRequest();
